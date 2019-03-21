e30128677cf6cd6df6e186433380e8daf0c75410
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-03-21 19:15:42 +0800

    12315

--- android-demo/app/src/main/java/com/caihongzhibo/phonelive/activity/LauncherActivity.java
+++ android-demo/app/src/main/java/com/caihongzhibo/phonelive/activity/LauncherActivity.java
@@ -1,5 +1,8 @@
 package com.caihongzhibo.phonelive.activity;
 
+import android.util.Log;
+import com.caihongzhibo.phonelive.bean.IParray;
+import com.google.gson.Gson;
 import com.lzy.okgo.model.Response;
 import com.caihongzhibo.phonelive.AppConfig;
 import com.caihongzhibo.phonelive.AppContext;
@@ -14,12 +17,25 @@ import com.caihongzhibo.phonelive.utils.L;
 import com.caihongzhibo.phonelive.utils.LoginUtil;
 import com.caihongzhibo.phonelive.utils.SharedPreferencesUtil;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import cn.sharesdk.framework.ShareSDK;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * 启动页面
  */
 public class LauncherActivity extends AbsActivity {
+    List<String> list= new ArrayList<>();
+    IParray  iParray;
 
     @Override
     protected int getLayoutId() {
@@ -28,18 +44,110 @@ public class LauncherActivity extends AbsActivity {
 
     @Override
     protected void main() {
-        //开启倒计时
-        startCountDown();
-        //初始化http
-        HttpUtil.init();
-        //初始化极光推送
-        JPushUtil.init();
-        //初始化sharedSdk
-        ShareSDK.initSDK(AppContext.sInstance);
-        //初始化IM
-        IMUtil.getInstance().init(IMUtil.JIM);
-        AppConfig.getInstance().setLaunched(true);
+        //获取ip集合
+        getIparrist();
+
     }
+
+    private void getIparrist() {
+        String url = "http://api.dd7666.com/index.php?r=index/getapi";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()//默认就是GET请求，可以不写
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("123", "onFailure: ");
+            }
+            @Override
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                String s=response.body().string();
+                iParray=new Gson().fromJson(s,IParray.class);
+
+                for (int i=0;i<iParray.getData().size();i++){
+                    list.add(iParray.getData().get(i)+"/api/public/index.php?service=Home.apiTest");
+                }
+                for (int m=0;m<list.size();m++){
+                    sendHttpRequest(list.get(m),m);
+                }
+            }
+
+
+        });
+
+    }
+
+
+    public SimpleDateFormat dfs = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");//设置日期格式
+    String time_string = "";
+    //请求网址响应
+    public String sendHttpRequest(final String address, final int i) {
+
+        new Thread(new Runnable() {
+            long between = 0;
+            String date2 = "";
+            public void run() {
+
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(9999);
+                    connection.setReadTimeout(9999);
+                    final String date1 = dfs.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
+                    connection.connect();
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == 200) {
+                        date2 = dfs.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
+                        Date begin = dfs.parse(date1);
+                        Date end = dfs.parse(date2);
+                        between = Math.abs((end.getTime() - begin.getTime()));// 得到两者的毫秒数
+                        Log.e("between",between+"");
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AppConfig.HOST=iParray.getData().get(i);
+                                //开启倒计时
+                                startCountDown();
+                                //初始化http
+                                HttpUtil.init();
+                                //初始化极光推送
+                                JPushUtil.init();
+                                //初始化sharedSdk
+                                ShareSDK.initSDK(AppContext.sInstance);
+                                //初始化IM
+                                IMUtil.getInstance().init(IMUtil.JIM);
+                                AppConfig.getInstance().setLaunched(true);
+                            }
+                        });
+
+
+
+
+                    } else {
+                        Log.e("between","失敗了。。。。。。");
+                    }
+
+                } catch (Exception e) {
+                    Log.e("between","發生錯誤了。。。。。。"+e.toString());
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+
+                }
+            }
+        }).start();
+        return time_string;
+    }
+
+
+
 
     /**
      * 启动定时器，3秒后跳转
